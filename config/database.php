b910c823fb588be03d74220bbfc688f1e1be1ed8
David Joan Tataje Mendoza <davidtataje@gmail.com>
2015-07-21 10:54:39 -0500

    configuracion de los entornos de produccion

--- config/database.php
+++ config/database.php
@@ -124,9 +124,11 @@ return [
 		'cluster' => false,
 
 		'default' => [
-			'host'     => '127.0.0.1',
-			'port'     => 6379,
-			'database' => 0,
+			'host'     => env('REDIS_SERVER', '127.0.0.1'),
+			'port'     => env('REDIS_PORT', '6379'),
+			'username' => env('REDIS_USERNAME', 'David'),
+			'password' => env('REDIS_PASSWORD', '1234'),
+			'database' => env('REDIS_DB', '0'),
 		],
 
 	],
